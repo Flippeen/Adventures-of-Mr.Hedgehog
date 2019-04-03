119a2be7ba8cbb2d5b02947eb53767d8168e518f
AlexanderMalthed <Kattenlevererar@gmail.com>
2019-04-03 17:20:58 +0200

    Kap6 påbörjad whiteboxing och environment design.

---
+++ The Adventures of Mr Hedgehog/Assets/Game/Scripts/Minigames/Kap8Minigame/StoneRolling.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneRolling : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 1000 * Time.deltaTime, ForceMode.VelocityChange);
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.tag == "RiverWalls")
+            rb.AddForce((c.GetContact(0).normal + c.transform.forward).normalized * 500 * Time.deltaTime, ForceMode.VelocityChange);
+    }
+    [SerializeField] float gravityDown;
+    [SerializeField] LayerMask ground;
+    void Update()
+    {
+        Debug.DrawRay(transform.position, Vector3.down * (transform.localScale.y / 2));
+        if (!Physics.Raycast(transform.position, Vector3.down, transform.localScale.y / 2, ground))
+        {
+            Debug.Log("In Air");
+            rb.velocity += Vector3.down * gravityDown * Time.deltaTime;
+        }
+    }
+}
