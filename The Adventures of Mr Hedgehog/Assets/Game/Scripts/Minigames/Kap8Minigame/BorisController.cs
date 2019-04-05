b6b0754c966735b5f85928d935e5c45fe0c03246
Flippeen <filip.k.lindgren@gmail.com>
2019-04-05 08:54:36 +0200

    Kap 8 Björn
    Nu kan han göra sin charge och sen sitt rytande, testa gärna.

--- The Adventures of Mr Hedgehog/Assets/Game/Scripts/Minigames/Kap8Minigame/BorisController.cs
+++ The Adventures of Mr Hedgehog/Assets/Game/Scripts/Minigames/Kap8Minigame/BorisController.cs
@@ -13,35 +13,31 @@ public class BorisController : MonoBehaviour
         aS = GetComponent<AudioSource>();
         StartCoroutine(CheckForPlayer());
     }
-    bool roaring;
-    bool canSeePlayer;
-    void Update()
-    {
-        if (roaring && aS.isPlaying && !canSeePlayer)
-        {
-            if (Physics.Raycast(transform.position, playerPos.position - transform.position, out hitInfo, 100) &&
-            hitInfo.transform.tag == "Player")
-            {
-                canSeePlayer = true;
-                StartCoroutine(BlowHimAway(hitInfo));
-            }
-        }
-    }
-
     RaycastHit hitInfo;
     [SerializeField] AudioClip[] bearSounds;
+    bool canSeePlayer;
     IEnumerator CheckForPlayer()
     {
         aS.clip = bearSounds[0];
         aS.Play();
         yield return new WaitForSeconds(bearSounds[0].length);
-        aS.Stop();
-
-        roaring = true;
+        
         aS.clip = bearSounds[1];
         aS.Play();
+        while (aS.isPlaying)
+        {
+            if (Physics.Raycast(transform.position, playerPos.position - transform.position, out hitInfo, 100) && !canSeePlayer)
+            {
+                canSeePlayer = true;
+                StartCoroutine(BlowHimAway(hitInfo));
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+            yield return new WaitForEndOfFrame();
+        }
         yield return new WaitForSeconds(bearSounds[1].length);
-        roaring = false;
 
         yield return new WaitForSeconds(3);
         StartCoroutine(CheckForPlayer());
@@ -58,14 +54,12 @@ public class BorisController : MonoBehaviour
                 aS.clip = bearSounds[1];
                 aS.Play();
             }
-            rb.velocity += (playerPos.position - transform.position).normalized * 50 * Time.deltaTime;
+            rb.velocity += (playerPos.position - transform.position).normalized * 100 * Time.deltaTime;
             yield return new WaitForSeconds(0.1f);
         }
         if(Vector3.Distance(playerPos.position, transform.position) >= 100)
         {
             hit.transform.GetComponent<PlayerMovement>().FreezePlayerMovement(false, transform);
-            roaring = false;
-            canSeePlayer = false;
         }
     }
 }
